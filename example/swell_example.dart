1944eb3d56036ff0e82fc7416ea4014c2425137a
Bill Nizeyimana <bni@escaux.com>
2022-07-25 23:11:26 +0200

    implement retrofit

--- example/swell_example.dart
+++ example/swell_example.dart
@@ -1,6 +1,30 @@
+import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:swell/src/api/swell_api.dart';
 import 'package:swell/swell.dart';
 
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+
 void main() {
-  var awesome = Swell();
-  print('awesome: ${awesome.isAwesome}');
+  Swell.init(store: 'malin-malin', key: 'pk_ZAw7FXsVVqG6yinHfaiEx34wFAsk0eDo');
+  print(Swell.instance.url.toString());
+  print(Swell.instance.key);
+  var b64Key = base64.encode(utf8.encode(Swell.instance.key));
+  final dio = Dio(); // Provide a dio instance
+  dio.options.headers["accept"] = "application/json";
+  dio.options.headers["content-Type"] = "application/json";
+  dio.options.headers["authorization"] = 'Basic $b64Key';
+  dio.interceptors.add(PrettyDioLogger(
+      requestHeader: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: false,
+      error: true,
+      compact: true,
+      maxWidth: 90));
+  final client = SwellApi(dio, baseUrl: Swell.instance.url.toString());
+
+  client.getProducts(null).then((it) => print(it));
 }
--- lib/src/swell_base.dart
+++ lib/src/swell_base.dart
@@ -1,6 +1,24 @@
 // TODO: Put public facing types in this file.
 
-/// Checks if you are awesome. Spoiler: you are.
+late final Swell _swell;
+
 class Swell {
-  bool get isAwesome => true;
+  final Uri _url;
+  final String _store;
+  final String _key;
+
+  get url => _url;
+  get store => _store;
+  get key => _key;
+
+  static get instance => _swell;
+
+  //Swell(this.store, this.key);
+  static init({required String store, required String key}) =>
+      _swell = Swell._(store: store, key: key);
+
+  Swell._({required String store, required String key})
+      : _url = Uri.parse('https://$store.swell.store/api'),
+        _key = key,
+        _store = store;
 }
